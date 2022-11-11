94aae258e97a71e02cad0082cc881e9c697622aa
ShevchukIhor <bluesjazz666@gmail.com>
2022-11-11 13:05:28 +0200

    added directory text_to_html with gem file

---
+++ HW3/text_to_html/lib/text_to_html.rb
@@ -0,0 +1,43 @@
+require 'sanitize'
+
+module ToHtml
+
+  attr_accessor :to_body
+
+  def initialize
+    @to_body
+  end
+
+  def read_rb_file(file_name, bypass_html: true)
+
+    if File.exist?("#{file_name}")
+    @to_body = File.read("#{file_name}")
+    @to_body = bypass_html ? Sanitize.fragment(@to_body) : @to_body
+    else
+      p 'File not found'
+    end
+
+  end
+
+  def insert_to_html(name = "index")
+    File.open("#{name}.html", "w") {|file| file.write(document)}
+  end
+
+  private
+
+  def document(bypass_html: true)
+    "<!DOCTYPE html>
+<html lang=\"en\">
+  <head>
+    <meta charset=\"UTF-8\">
+    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
+    <title></title>
+  </head>
+  <body>
+    <div>#{@to_body = bypass_html ? Sanitize.fragment(@to_body) : @to_body}</div>
+  </body>
+</html>"
+  end
+
+end
---
+++ HW3/text_to_html/text_to_html.gemspec
@@ -0,0 +1,8 @@
+Gem::Specification.new do |s|
+  s.name        = 'text_to_html'
+  s.version     = '0.0.1'
+  s.licenses    = ['MIT']
+  s.summary     = "insert content to html file"
+  s.authors     = ["Shevchuk Ihor"]
+  s.files       = Dir["lib/**/*.rb"]
+end
