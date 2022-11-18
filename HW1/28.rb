870b62861a7a7ffa1dc5a9bbf8f7caa868ba704e
ShevchukIhor <bluesjazz666@gmail.com>
2022-11-18 11:31:55 +0200

    update code

--- HW1/28.rb
+++ HW1/28.rb
@@ -3,6 +3,6 @@ array = [100, 12, 10, -11, -2, 0, 45, 120, 55, 68, 70, 96, 2, 8]
 p 'Дан целочисленный массив:'
 p array
 p 'Найти индекс первого экстремального элемента.'
-p "result max: #{ array.index(array.max) }"
-p "result min: #{ array.index(array.min) }"
+p "result max: #{array.index(array.max)}"
+p "result min: #{array.index(array.min)}"
 p '______________________________________________________________________________________________'
