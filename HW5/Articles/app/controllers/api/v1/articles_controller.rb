076e2e2eb3fe747003d097b4ee33788bd14d69b6
ShevchukIhor <bluesjazz666@gmail.com>
2022-11-22 22:23:20 +0200

    update code

--- HW5/Articles/app/controllers/api/v1/articles_controller.rb
+++ HW5/Articles/app/controllers/api/v1/articles_controller.rb
@@ -1,31 +1,31 @@
 class Api::V1::ArticlesController < ApplicationController
   def index
-    articles = Article.order('created_at DESC')
+    @articles = Article.order('created_at DESC')
 
-    if articles
-      render json: { status: 'SUCCESS', message: 'Fetched all the articles successfully', data: articles }, status: :ok
+    if @articles
+      render json: { status: 'SUCCESS', message: 'Fetched all the articles successfully', data: @articles }, status: :ok
     else
-      render json: articles.errors, status: :bad_request
+      render json: @articles.errors, status: :bad_request
     end
 
   end
 
   def create
-    articles = Article.new(articles_params)
+    @articles = Article.new(articles_params)
 
-    if articles.save
-      render json: { status: 'SUCCESS', message: 'Articles was created successfully!', data: articles }, status: :created
+    if @articles.save
+      render json: { status: 'SUCCESS', message: 'Articles was created successfully!', data: @articles }, status: :created
     else
-      render json: articles.errors, status: :unprocessable_entity
+      render json: @articles.errors, status: :unprocessable_entity
     end
 
   end
 
   def show
-    articles = Article.find(params[:id])
+    @articles = Article.find(params[:id])
 
-    if articles
-      render json: { data: articles }, state: :ok
+    if @articles
+      render json: { data: @articles }, state: :ok
     else
       render json: { message: 'Articles could not be found' }, status: :bad_request
     end
@@ -33,9 +33,9 @@ class Api::V1::ArticlesController < ApplicationController
   end
 
   def destroy
-    articles = Article.find(params[:id])
+    @articles = Article.find(params[:id])
 
-    if articles.destroy!
+    if @articles.destroy!
       render json: { message: 'Articles was deleted successfully' }, status: :ok
     else
       render json: { message: 'Articles does not exist' }, status: :bad_request
@@ -44,13 +44,14 @@ class Api::V1::ArticlesController < ApplicationController
   end
 
   def update
-    articles = Article.find(params[:id])
+    @articles = Article.find(params[:id])
 
-    if articles.update!(articles_params)
-      render json: { message: 'Articles was updated successfully', data: articles }, status: :ok
+    if @articles.update!(articles_params)
+      render json: { message: 'Articles was updated successfully', data: @articles }, status: :ok
     else
       render json: { message: 'Articles cannot be updated' }, status: :unprocessable_entity
     end
+
   end
 
   private
