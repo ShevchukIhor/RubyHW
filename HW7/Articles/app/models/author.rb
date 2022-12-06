d58d75848e5ded9517ee08a8a2871b449dc77dee
ShevchukIhor <bluesjazz666@gmail.com>
2022-12-06 23:56:50 +0200

    delete HW7

--- HW7/Articles/app/models/author.rb
+++
@@ -1,6 +0,0 @@
-class Author < ApplicationRecord
-  has_many :articles, dependent: :destroy
-  has_many :comments, dependent: :destroy
-
-  validates :name, presence: true
-end
--- HW7/Articles/db/migrate/20221206195257_add_status_to_article.rb
+++
@@ -1,5 +0,0 @@
-class AddStatusToArticle < ActiveRecord::Migration[7.0]
-  def change
-    add_column :articles, :status, :integer
-  end
-end
