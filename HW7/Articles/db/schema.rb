5c706d220d2ec30125ec8ba0e37a53eb288faf72
ShevchukIhor <bluesjazz666@gmail.com>
2022-12-07 12:43:07 +0200

    HW7, edited seeds.rb, added migration to article

--- HW7/Articles/db/schema.rb
+++ HW7/Articles/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_06_195257) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_07_102632) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,6 +30,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_06_195257) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "status"
+    t.bigint "author_id", null: false
+    t.index ["author_id"], name: "index_articles_on_author_id"
   end
 
   create_table "authors", force: :cascade do |t|
@@ -57,6 +59,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_06_195257) do
 
   add_foreign_key "article_tags", "articles"
   add_foreign_key "article_tags", "tags"
+  add_foreign_key "articles", "authors"
   add_foreign_key "comments", "articles"
   add_foreign_key "comments", "authors"
 end
