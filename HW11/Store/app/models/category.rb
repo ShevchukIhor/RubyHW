b58bd74e06003704b47b13c69c9eea3b5756f668
ShevchukIhor <bluesjazz666@gmail.com>
2023-01-11 13:36:55 +0200

    edit category, product model

--- HW11/Store/app/models/category.rb
+++ HW11/Store/app/models/category.rb
@@ -2,5 +2,6 @@
 
 class Category < ApplicationRecord
   has_many :products, dependent: :destroy
+
   validates :title, presence: true, uniqueness: true
 end
--- HW11/Store/app/models/product.rb
+++ HW11/Store/app/models/product.rb
@@ -2,5 +2,7 @@
 
 class Product < ApplicationRecord
   belongs_to :category
+  has_many   :line_items, dependent: :nullify
+
   validates :name, presence: true
 end
