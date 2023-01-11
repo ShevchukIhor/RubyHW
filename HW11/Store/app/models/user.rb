3ac9c40e4e02c9733bf89a9822b5ef505d1e7611
ShevchukIhor <bluesjazz666@gmail.com>
2023-01-11 13:39:52 +0200

    add association to user model

--- HW11/Store/app/models/user.rb
+++ HW11/Store/app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+
+  has_many :orders, dependent: :nullify
+  has_one :cart, dependent: :nullify
 end
