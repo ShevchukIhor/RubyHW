cda0ae50e42019dc37ee784f733143c52c3039d1
ShevchukIhor <bluesjazz666@gmail.com>
2023-01-21 09:59:25 +0200

    move logic to one update method from add_quantity and reduce_quantity method

--- HW12/Store/app/controllers/line_items_controller.rb
+++ HW12/Store/app/controllers/line_items_controller.rb
@@ -2,7 +2,7 @@
 
 class LineItemsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_line_item, only: %i[destroy add_quantity reduce_quantity]
+  before_action :set_line_item, only: %i[update destroy]
 
   def create
     product = Product.find(params[:product_id])
@@ -10,26 +10,19 @@ class LineItemsController < ApplicationController
     redirect_to cart_path, notice: "#{product.name} added to cart"
   end
 
-  def destroy
-    set_line_item
-    @line_item.destroy
-    redirect_back(fallback_location: current_cart)
-  end
-
-  def add_quantity
-    @line_item.quantity += 1
+  def update
+    if params[:add] == 'increase'
+      @line_item.quantity += 1
+    else
+      @line_item.quantity > 1 ? (@line_item.quantity -= 1) : @line_item.destroy
+    end
     @line_item.save
     redirect_back(fallback_location: current_cart)
   end
 
-  def reduce_quantity
-    if @line_item.quantity > 1
-      @line_item.quantity -= 1
-      @line_item.save
-      redirect_back(fallback_location: current_cart)
-    elsif @line_item.quantity.eql?(1)
-      destroy
-    end
+  def destroy
+    @line_item.destroy
+    redirect_back(fallback_location: current_cart)
   end
 
   private
