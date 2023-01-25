fd8329546937eb874a3eb60c8a9bd9581821ae0b
ShevchukIhor <bluesjazz666@gmail.com>
2023-01-25 17:11:44 +0200

    add association to active_storage, create attached variant for image

--- HW13/Store/app/models/product.rb
+++ HW13/Store/app/models/product.rb
@@ -24,6 +24,9 @@
 class Product < ApplicationRecord
   belongs_to :category, dependent: :destroy
   has_many   :line_items, dependent: :destroy
-
+  has_one_attached :image do |attachable|
+    attachable.variant :small, resize_to_limit: [50, 50]
+    attachable.variant :main, resize_to_limit: [200, 200]
+  end
   validates :name, presence: true
 end
